f4e39665d18a9e7e39da4b7b431cf1ad6604cfdb
Kamlesh Katpara <mekamleshk@gmail.com>
2019-09-24 06:46:03 +0530

    upload and insert

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path');
-const multer  = require('multer')
+const multer = require('multer')
 const port = 3000
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema
@@ -11,76 +11,61 @@ const connected = chalk.bold.cyan;
 const error = chalk.bold.yellow;
 const server = chalk.bold.red;
 const fs = require('fs')
-const async = require('async')
+
+
 mongoose.connect(dburl, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log(connected(`Mongoose connection is open to  ${dburl}`)))
-    .catch((err) => console.log(error(`Mongoose connection has occured ${err} error`)));
+  .then(() => console.log(connected(`Mongoose connection is open to  ${dburl}`)))
+  .catch((err) => console.log(error(`Mongoose connection has occured ${err} error`)));
 
 mongoose.set('useFindAndModify', false);
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './uploads')
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.originalname)
-    }
-  })
-  
+  destination: function (req, file, cb) {
+    cb(null, './uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+
 const upload = multer({ storage: storage }).single('datafile')
+
+const DataSchema = new Schema({ ts: Number, value: Number })
 
 app.get('/', (req, res) => res.sendFile(path.resolve('./index.html')))
 
 app.post('/upload', function (req, res) {
-    upload(req, res, function (err) {
-        if (err instanceof multer.MulterError) {
-          console.log('A Multer error occurred when uploading');
-        } else if (err) {
-            console.log('An unknown error occurred when uploading');
-        }
+  upload(req, res, function (err) {
+    if (err instanceof multer.MulterError) {
+      console.log('A Multer error occurred when uploading');
+    } else if (err) {
+      console.log('An unknown error occurred when uploading');
+    }
 
-        const absolutePath = path.join('./uploads/', req.file.originalname);
-        const jsonString = fs.readFileSync(absolutePath, "utf-8");
-        const jsonObject = JSON.parse(jsonString);
+    const stream = fs.readFileSync(`./uploads/${req.file.originalname}`, 'utf-8');
+    const jsonData = JSON.parse(stream);
 
-        let DataSchema = new Schema({
-            ts: Number,
-            value: Number
-        })
-        
-        const Data = mongoose.model('Data', DataSchema, 'temperatures');
+    const Data = mongoose.model('Data', DataSchema, req.file.originalname.substr(0, req.file.originalname.indexOf('.')));
 
-        Data.collection.insert(jsonObject)  
-        .then((res) => {
-            console.log("insert sampleCollection result ", res);
-        })
-        .catch(err => {
-            console.log("bulk insert sampleCollection error ", err);
-        });
-        
-        res.send(req.file)
-    })
+    for (let i = 0; i < jsonData.length; i += 10000) {
+      Data.collection.insertMany(jsonData.slice(i, i + 10000));
+    }
+
+    res.send(req.file)
+  })
 })
 
-app.get('/read', async (req, res) => {
-  const jsonString = JSON.parse(fs.readFileSync('./uploads/THERM0001.json', "utf-8"));
+app.get('/read', function (req, res) {
 
-  let DataSchema = new Schema({
-    ts: Number,
-    value: Number
-  })
+  const stream = fs.readFileSync('./uploads/THERM0001.json', 'utf-8');
+  const jsonData = JSON.parse(stream);
 
-  const Data = mongoose.model('Data', DataSchema, 'temperatures');
-
-  timer = (new Date()).getTime() / 1000;
-
-  for (let i = 0; i < jsonString.length; i += 100000) {
-    if( i % 100000 === 0 ) {
-      await Data.collection.insert(jsonString.slice(i, i + 100000), {ordered: false});
-    }
+  for (let i = 0; i < jsonData.length; i += 10000) {
+    Data.collection.insertMany(jsonData.slice(i, i + 10000));
   }
 
-  res.send('Done')
+  res.send('Done');
+
 });
 
 app.listen(port, () => console.log(server(`Thermometer app listening on port ${port}!`)))
